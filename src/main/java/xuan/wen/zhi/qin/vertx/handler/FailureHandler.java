52eff511d20684c3433fc3263862cb8c10813cbf
qinzhiwenxuan <qinzhiwenxuan@gmail.com>
2017-08-02 09:49:53 +0800

    version : 1.004
    *) 修改http请求异常处理
    
    Signed-off-by: QinZhiWenXuan <qinzhiwenxuan@gmail.com>

--- src/main/java/xuan/wen/zhi/qin/vertx/handler/FailureHandler.java
+++ src/main/java/xuan/wen/zhi/qin/vertx/handler/FailureHandler.java
@@ -12,8 +12,15 @@ public class FailureHandler {
 
     public void httpFailureHandler(RoutingContext failureHandler) {
         Throwable failure = failureHandler.failure();
-        JsonObject error = new JsonObject().put("code", 500).put("message", "Internal Server Error : " + failure.getLocalizedMessage());
-        logger.error("http error :", failure);
-        failureHandler.response().putHeader("Content-Type", "application/json").end(error.toBuffer());
+        if (null != failure) {
+            JsonObject error = new JsonObject().put("code", 500).put("message", "Internal Server Error : " + failure.getLocalizedMessage());
+            logger.error("http error :", failure);
+            failureHandler.response().putHeader("Content-Type", "application/json").end(error.toBuffer());
+            return;
+        } else {
+            return;
+        }
+
+
     }
 }

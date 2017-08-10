dfd96fe66f82679af217630a081c7195deb95d07
qinzhiwenxuan <qinzhiwenxuan@gmail.com>
2017-08-10 11:01:06 +0800

    version : 1.009
    *) 修改http请求异常处理
       使用ErrorHandler处理异常
    
    Signed-off-by: QinZhiWenXuan <qinzhiwenxuan@gmail.com>

--- src/main/java/xuan/wen/zhi/qin/core/vertx/server/VertxServer.java
+++ src/main/java/xuan/wen/zhi/qin/core/vertx/server/VertxServer.java
@@ -2,6 +2,7 @@ package xuan.wen.zhi.qin.core.vertx.server;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.ErrorHandler;
 import io.vertx.ext.web.handler.StaticHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,6 +19,7 @@ public class VertxServer extends AbstractVerticle {
 
     @Autowired
     private FailureHandler failureHandler;
+
     /**
      * If your verticle does a simple, synchronous start-up then override this method and put your start-up
      * code in there.
@@ -31,9 +33,12 @@ public class VertxServer extends AbstractVerticle {
         this.routeStatic();
         this.vertx.createHttpServer().requestHandler(this.router::accept).listen(this.port);
     }
+
     private void httpFailureHandler() {
-        this.router.route().failureHandler(this.failureHandler::httpFailureHandler);
+//        this.router.route().failureHandler(this.failureHandler::httpFailureHandler);
+        this.router.route().failureHandler(ErrorHandler.create("webroot/error.html" ,true));
     }
+
     private void routeStatic() {
         this.router.route().handler(StaticHandler.create());
     }
